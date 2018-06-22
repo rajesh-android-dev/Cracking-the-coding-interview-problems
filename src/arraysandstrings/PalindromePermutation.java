904ac5885ae32eb03a1a9aa810ae1e0c2f1aaea4
Mac Mini <macmini@Macs-Mac-mini.local>
2018-06-22 13:18:48 +0530

    Palindrome permutation program added

--- src/arraysandstrings/PalindromePermutation.java
+++ src/arraysandstrings/PalindromePermutation.java
@@ -17,27 +17,40 @@ public class PalindromePermutation {
     }
 
     private static boolean isPalindromePerm(String str) {
-        int[] val = new int[128];
-        for (char c : str.toCharArray()){
-            if (c != ' '){
-                if (val[c] == 0){
-                    val[c]++;
-                }
-                else {
-                    val[c]--;
-                }
-            }
-        }
-        int centerVal = 0;
-        for (int i : val){
-            if (i == 1){
-                centerVal++;
-                if (centerVal > 1)
-                {
+        int[] table = buildCharFrequencyTable(str);
+        return checkMaxOneOdd(table);
+    }
+
+    private static boolean checkMaxOneOdd(int[] table){
+        boolean foundOdd = false;
+        for (int i : table){
+            if (i % 2 == 1){
+                if (foundOdd){
                     return false;
                 }
+                foundOdd = true;
             }
         }
         return true;
+    }
+
+    private static int getCharNumber(char c){
+        int a = Character.getNumericValue('a');
+        int z = Character.getNumericValue('z');
+        int val = Character.getNumericValue(c);
+        if (a <= val && val <= z){
+            return val- a;
+        }
+        return -1;
+    }
+    private static int[] buildCharFrequencyTable(String phrase){
+        int[] table = new int[Character.getNumericValue('z') - Character.getNumericValue('a')+1];
+        for (char c : phrase.toCharArray()){
+            int x = getCharNumber(c);
+            if (x != -1){
+                table[x]++;
+            }
+        }
+        return table;
     }
 }
