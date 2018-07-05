ce9238e27da97dbc7ab4a3611db6da31103b65e6
rajesh-android-dev <macmini@Macs-Mac-mini.local>
2018-07-05 11:49:28 +0530

    New tree program added

---
+++ src/TreesAndGraphs/Successor.java
@@ -0,0 +1,31 @@
+package TreesAndGraphs;
+
+/**
+ * Successor: Write an algorithm to find the "next" node (i.e., in-order successor) of a given node in a
+ * binary search tree. You may assume that each node has a link to its parent.
+ */
+public class Successor {
+    TreeNode inOrderSucc(TreeNode n) {
+        if (n == null) return null;
+
+        if (n.right != null) {
+            return leftMostChild(n.right);
+        } else {
+            TreeNode q = n;
+            TreeNode x = q.parent;
+            while (x != null && x.left != q) {
+                q = x;
+                x = x.parent;
+            }
+            return x;
+        }
+    }
+
+    private TreeNode leftMostChild(TreeNode n) {
+        if (n == null) return null;
+        while (n.left != null) {
+            n = n.left;
+        }
+        return n;
+    }
+}
--- src/TreesAndGraphs/TreeNode.java
+++ src/TreesAndGraphs/TreeNode.java
@@ -4,6 +4,7 @@ public class TreeNode {
     public int value;
     public TreeNode left;
     public TreeNode right;
+    public TreeNode parent;
 
     public TreeNode(int val) {
         value = val;
